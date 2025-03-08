884798cd8cc8636646dbc372f7afdad385725ea7
thequinn <200894372+thequinndev@users.noreply.github.com>
2025-03-08 11:30:03 +1100

    chore: include more schema examples for the next documentation step

--- examples/route-manager/endpoints/index.ts
+++ examples/route-manager/endpoints/index.ts
@@ -1,15 +1,33 @@
 import { makeEndpoint, endpointGroup } from '@thequinndev/route-manager/endpoint'
 import { z } from 'zod'
 
+const errorSchema = z.object({
+    code: z.string(),
+    message: z.string(),
+})
+
+const apiDocumentationSchema = z.object({
+    apiVersion: z.string(),
+    apiStatus: z.enum(['active', 'deprecated', 'inactive']),
+    apiDocumentation: z.string().url(),
+})
+
+const getApiDocumentation = makeEndpoint({
+    operationId: 'getApiDocumentation',
+    path: '/',
+    method: 'get',
+    returns: {
+        200: apiDocumentationSchema,
+        400: errorSchema.array(),
+        404: errorSchema.array(),
+        500: z.literal('Internal Server Error')
+    }
+})
+
 const userSchema = z.object({
     id: z.number(),
     name: z.string(),
     description: z.string(),
-})
-
-const errorSchema = z.object({
-    code: z.string(),
-    message: z.string(),
 })
 
 const getUserById = makeEndpoint({
@@ -29,6 +47,64 @@ const getUserById = makeEndpoint({
     }
 })
 
+const searchUsers = makeEndpoint({
+    operationId: 'searchUsers',
+    path: '/users',
+    method: 'get',
+    accepts: {
+        query: z.object({
+            name: userSchema.shape.name,
+            description: userSchema.shape.description
+        }).partial(),
+    },
+    returns: {
+        200: userSchema.array(),
+        400: errorSchema.array(),
+        404: errorSchema.array(),
+        500: z.literal('Internal Server Error')
+    }
+})
+
+const createUser = makeEndpoint({
+    operationId: 'createUser',
+    path: '/users',
+    method: 'post',
+    accepts: {
+        body: userSchema.omit({id: true})
+    },
+    returns: {
+        200: userSchema,
+        400: errorSchema.array(),
+        404: errorSchema.array(),
+        500: z.literal('Internal Server Error')
+    }
+})
+
+const updateUser = makeEndpoint({
+    operationId: 'updateUser',
+    path: '/users/{userId}',
+    method: 'put',
+    accepts: {
+        path: z.object({
+            userId: userSchema.shape.id
+        }),
+        body: userSchema
+    },
+    returns: {
+        200: userSchema,
+        400: errorSchema.array(),
+        404: errorSchema.array(),
+        500: z.literal('Internal Server Error')
+    }
+})
+
+export const apiDocumentationEndpoints = endpointGroup([
+    getApiDocumentation
+])
+
 export const userEndpoints = endpointGroup([
-    getUserById
+    getUserById,
+    searchUsers,
+    createUser,
+    updateUser
 ])
