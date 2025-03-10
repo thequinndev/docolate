4d0e5051553edfe0eea69534fb398c60304f5dfd
thequinn <200894372+thequinndev@users.noreply.github.com>
2025-03-10 13:44:13 +1100

    chore: include more status codes

--- src/route-manager/endpoint/index.ts
+++ src/route-manager/endpoint/index.ts
@@ -2,8 +2,62 @@ import { z } from "zod";
 
 type Methods = "get" | "post" | "put" | "patch" | "delete";
 
-export type ValidSuccessCodes = 200 | 201;
-export type ValidErrorCodes = 400 | 401 | 403 | 404 | 500;
+export type ValidSuccessCodes = 
+  200  // OK
+| 201  // Created
+| 202  // Accepted
+| 203  // Non-Authoritative Information
+| 204  // No Content
+| 205  // Reset Content
+| 206  // Partial Content
+| 207  // Multi-Status (WebDAV)
+| 208  // Already Reported (WebDAV)
+| 226  // IM Used (HTTP Delta encoding)
+
+
+export type ValidErrorCodes = 
+  400  // Bad Request
+| 401  // Unauthorized
+| 402  // Payment Required
+| 403  // Forbidden
+| 404  // Not Found
+| 405  // Method Not Allowed
+| 406  // Not Acceptable
+| 407  // Proxy Authentication Required
+| 408  // Request Timeout
+| 409  // Conflict
+| 410  // Gone
+| 411  // Length Required
+| 412  // Precondition Failed
+| 413  // Payload Too Large
+| 414  // URI Too Long
+| 415  // Unsupported Media Type
+| 416  // Range Not Satisfiable
+| 417  // Expectation Failed
+| 418  // I'm a teapot (RFC 2324)
+| 421  // Misdirected Request
+| 422  // Unprocessable Entity (WebDAV)
+| 423  // Locked (WebDAV)
+| 424  // Failed Dependency (WebDAV)
+| 425  // Too Early
+| 426  // Upgrade Required
+| 427  // Unassigned (not used)
+| 428  // Precondition Required
+| 429  // Too Many Requests
+| 431  // Request Header Fields Too Large
+| 451  // Unavailable For Legal Reasons
+| 500  // Internal Server Error
+| 501  // Not Implemented
+| 502  // Bad Gateway
+| 503  // Service Unavailable
+| 504  // Gateway Timeout
+| 505  // HTTP Version Not Supported
+| 506  // Variant Also Negotiates
+| 507  // Insufficient Storage (WebDAV)
+| 508  // Loop Detected (WebDAV)
+| 510  // Not Extended
+| 511  // Network Authentication Required
+
 export type ValidStatusCodes = ValidSuccessCodes | ValidErrorCodes;
 
 export type AnchoredSchema<T> = z.ZodType<any> & {
