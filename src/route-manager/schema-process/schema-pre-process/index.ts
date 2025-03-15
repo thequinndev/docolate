9211d2f29c61be8e4e355712063cfa841bb1a9ec
thequinn <200894372+thequinndev@users.noreply.github.com>
2025-03-15 11:17:33 +1100

    refactor: split out the schemaParser functions so v30 and v31 differences could be more easily tested

--- src/route-manager/schema-process/schema-pre-process/index.ts
+++ src/route-manager/schema-process/schema-pre-process/index.ts
@@ -1,3 +1,6 @@
+import { OASVersions } from "../../openapi"
+import { schema30 } from "../schema30"
+
 const removeUselessAnyOf = (schema: any) => {
     if (Array.isArray(schema.anyOf)) {
         schema.anyOf = schema.anyOf.filter((item: any) => {
@@ -15,9 +18,13 @@ const removeUselessAnyOf = (schema: any) => {
     return schema
 }
 
-export const schemaPreProcess = (schema: any) => {
+export const schemaPreProcess = (schema: any, version: OASVersions) => {
     if (schema.anyOf) {
         schema = removeUselessAnyOf(schema)
+    }
+
+    if (version === '3.0') {
+        schema = schema30(schema)
     }
 
     return schema
